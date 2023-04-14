d2e2c5db382d7b8c8aa72131a28b7437523c52eb
MichaelJS78 <130602348+MichaelJS78@users.noreply.github.com>
2023-04-14 19:41:50 +0100

    Add files via upload

--- Calc 2a.py
+++ Calc 2a.py
@@ -16,23 +16,58 @@ def subtraction(num1, num2):
 def divide(num1, num2):
     return num1 / num2
 
+print('Welcome to Basic Calculator')
 
+# Have reconfigured program to start with operation menu
 while True:
-    value1 = int(input("Enter 1st number: "))
-    value2 = int(input("Enter 2nd number: "))
 
-    print("Select operation 1-Division, 2-Multiplication, 3-Addition, 4-Subtraction")
+    print("Select operation 1-Division, 2-Multiplication, 3-Addition, 4-Subtraction, 5-Quit")
 
-    operation = int(input("Choose operation 1/2/3/4: "))
-    if operation == 1:
-        print(value1, "/", value2, "=", divide(value1, value2))
+    try:
+        operation = int(input("Choose operation 1/2/3/4/5: "))
+        if operation < 1 or operation > 5:
+            print('Number not valid. Please try again')
+            continue
+        elif operation == 5:
+            print('Thank you for using Basic Calculator')
+            break
+    except ValueError:
+        print('Please enter a number between 1 and 5')
+        continue
 
-    elif operation == 2:
-        print(value1, "*", value2, "=", multiplication(value1, value2))
-    elif operation == 3:
-        print(value1, "+", value2, "=", addition(value1, value2))
-    elif operation == 4:
-        print(value1, "-", value2, "=", subtraction(value1, value2))
+    while True:
+        try:
+            value1 = float(input ('Enter 1st number: '))
+            while True:
+                try:
+                    value2 = float(input ('Enter second number: '))
+                    break
+                except ValueError:
+                    print('Please enter a numerical value')
+                    continue
+        except ValueError:
+            print('Please enter a numerical value')
+            continue
 
-    else:
-        print("Enter correct operation")
+# Exception handling applied for division by 0
+        if operation == 1:
+            try:
+                value1 / value2
+            except ZeroDivisionError:
+                print(value1, "/", value2, "= 0")
+            else:
+                print(value1, "/", value2, "=", divide(value1, value2))
+
+
+
+        elif operation == 2:
+            print(value1, "*", value2, "=", multiplication(value1, value2))
+        elif operation == 3:
+            print(value1, "+", value2, "=", addition(value1, value2))
+        elif operation == 4:
+            print(value1, "-", value2, "=", subtraction(value1, value2))
+
+        print('Thank you')
+        break
+
+
