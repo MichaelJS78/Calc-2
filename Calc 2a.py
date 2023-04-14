b0a1944d8d3d0b06066f1f39a65d67d764261aae
MichaelJS78 <130602348+MichaelJS78@users.noreply.github.com>
2023-04-14 16:24:23 +0100

    Add files via upload

--- Calc 2a.py
+++ Calc 2a.py
@@ -24,7 +24,20 @@ while True:
 # Have included option to quit
     print("Select operation 1-Division, 2-Multiplication, 3-Addition, 4-Subtraction, 5-Quit")
 
-    operation = int(input("Choose operation 1/2/3/4/5: "))
+# Exception handling added for operation
+    try:
+        operation = int(input("Choose operation 1/2/3/4/5: "))
+        if operation < 1 or operation > 5:
+            print('Number not valid. Please try again')
+            continue
+        elif operation == 5:
+            print(quit())
+            break
+    except ValueError:
+        print('Please enter a number between 1 and 5')
+        continue
+
+
     if operation == 1:
         print(value1, "/", value2, "=", divide(value1, value2))
 
@@ -34,8 +47,4 @@ while True:
         print(value1, "+", value2, "=", addition(value1, value2))
     elif operation == 4:
         print(value1, "-", value2, "=", subtraction(value1, value2))
-    elif operation == 5:
-        print('Thank you for using Basic Calculator')
-        break
-    else:
-        print("Enter correct operation")
+
